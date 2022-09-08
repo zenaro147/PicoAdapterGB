64e3c07f13f794bb9b1a263f7a257985615b05b6
Rafael Zenaro <23667886+zenaror@users.noreply.github.com>
2022-09-08 12:24:04 -0300

    Update Files

--- pico_esp8266_at/src/flash_eeprom.h
+++ pico_esp8266_at/src/flash_eeprom.h
@@ -87,7 +87,7 @@ uint8_t ReadFlashConfig(uint8_t * buff){
 void SaveFlashConfig(uint8_t * buff){
     FormatFlashConfig();
     printf("Programming target region... ");
-    flash_range_program(FLASH_TARGET_OFFSET, dummy_config, FLASH_DATA_SIZE);
+    flash_range_program(FLASH_TARGET_OFFSET, buff, FLASH_DATA_SIZE);
     printf("Done.\n");
 }
 

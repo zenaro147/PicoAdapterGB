f15b77bf04c03a81c55f5b1a6b8e9d08b6426b13
Rafael Zenaro <rzoliveira93@gmail.com>
2023-04-26 15:06:27 -0300

    Better method to configure using Serial.

--- picow/src/gblink.h
+++ picow/src/gblink.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <mobile.h>
+#include "socket_impl.h"
+#include "flash_eeprom.h"
+
 #include "hardware/spi.h"
 #include "hardware/resets.h"
 
@@ -10,5 +14,15 @@
 #define PIN_SPI_SIN         16
 #define PIN_SPI_SCK         18
 #define PIN_SPI_SOUT        19 
+
+struct mobile_user {
+    struct mobile_adapter *adapter;
+    enum mobile_action action;
+    unsigned long picow_clock_latch[MOBILE_MAX_TIMERS];
+    uint8_t config_eeprom[FLASH_DATA_SIZE];
+    struct socket_impl socket[MOBILE_MAX_CONNECTIONS];
+    char number_user[MOBILE_MAX_NUMBER_SIZE + 1];
+    char number_peer[MOBILE_MAX_NUMBER_SIZE + 1];
+};
 
 void trigger_spi(spi_inst_t *spi, uint baudrate);

11c0cf2c8b9b3eb04b922157769420937a860d86
Rafael Zenaro <23667886+zenaror@users.noreply.github.com>
2023-11-21 15:30:51 -0300

    New Code Template

---
+++ pico_esp8266_at/src/config_menu.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "pico/stdlib.h"
+#include <mobile.h>
+
+int main_parse_addr(struct mobile_addr *dest, char *argv);
+void main_set_port(struct mobile_addr *dest, unsigned port);
+static bool main_parse_hex(unsigned char *buf, char *str, unsigned size);
+
+bool FindCommand(char * buf, char * target);
+void BootMenuConfig(void *user);

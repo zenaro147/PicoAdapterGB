ccca5ebd2aed7c39b6dfe09267a02a4c310611d0
Rafael Zenaro <rzoliveira93@gmail.com>
2023-11-21 15:36:39 -0300

    Update globals.h

--- pico_esp8266_at/src/globals.h
+++ pico_esp8266_at/src/globals.h
@@ -10,10 +10,10 @@
 
 //LED Config
 #define LED_PIN       		  	CYW43_WL_GPIO_LED_PIN
-#define LED_SET(A)    		  	(cyw43_arch_gpio_put(LED_PIN, (A)))
+#define LED_SET(A)    		  	(cgpio_put(LED_PIN, (A)))
 #define LED_ON        		  	LED_SET(true)
 #define LED_OFF       		  	LED_SET(false)
-#define LED_TOGGLE    		  	(cyw43_arch_gpio_put(LED_PIN, !cyw43_arch_gpio_get(LED_PIN)))
+#define LED_TOGGLE    		  	(gpio_put(LED_PIN, !gpio_get(LED_PIN)))
 
 //Time Config
 #define MKS(A)                  (A)
